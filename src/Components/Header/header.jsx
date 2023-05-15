f38a7a9796a4cd8c9e53eb3fc77da514b1e03a03
Thom <t.vanoort@nobears.nl>
2023-05-15 15:56:59 +0200

    fixed a lot of issues

---
+++ src/Components/Header/header.jsx
@@ -0,0 +1,48 @@
+import React, {useEffect, useState} from 'react';
+import './header.scss'
+
+const Header = props => {
+    const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
+
+    const handleThemeSwitch = () => {
+        theme === 'light' ? setTheme('dark') : setTheme('light')
+    }
+
+    const handleInput = (e) => {
+        const searchValue = e.target.value.toLowerCase();
+        const items = document.querySelectorAll('.item');
+
+        if (searchValue === '') return items.forEach(item => item.classList.remove('hidden'));
+
+        items.forEach(item => {
+            let itemName = item.dataset.name.toLowerCase();
+
+            if (itemName.includes(searchValue)) {
+                item.classList.remove('hidden');
+            } else {
+                item.classList.add('hidden');
+            }
+        })
+    }
+
+    useEffect(() => {
+        localStorage.setItem('theme', theme);
+        document.body.setAttribute('class', theme);
+    }, [theme]);
+
+    return (
+        <div className="header">
+            <div className="search">
+                <input
+                    type="input"
+                    onKeyUp={handleInput}
+                    placeholder={'Type here to search'}/>
+            </div>
+            <div className="theme-switch">
+                <img src={`./images/${theme === 'light' ? 'moon' : 'sun'}.svg`} alt="" onClick={handleThemeSwitch}/>
+            </div>
+        </div>
+    )
+}
+
+export default Header
--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {
-    createBrowserRouter, Navigate,
-    RouterProvider,
-} from "react-router-dom";
-import ErrorPage from "./error-page";
-import './index.css'
+import './index.scss'
 import Items from "./Components/Items/items";
-
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Items/>,
-        errorElement: <ErrorPage/>,
-        children: [
-            {
-                index: true,
-                path: "/:type",
-                element: <Items/>,
-                errorElement: <ErrorPage/>
-            }
-        ]
-    },
-]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <RouterProvider router={router}/>
+        <Items/>
     </React.StrictMode>
 )
